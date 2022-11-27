d28c19941eafcd473204caafe97885ba32b935c3
mamaciel <marcospaulofilho@hotmail.com>
2022-11-27 00:44:59 -0500

    Everything's working, needs more styling updates, add start times for games, add current quarter, add game ended

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ getAPI();
 //};
 
 async function getAPI() {
+
+    const date = new Date();
+    let day = date.getDate() + 1;
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
     const options = {
         method: 'GET',
         headers: {
@@ -11,12 +17,10 @@ async function getAPI() {
             'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
         }
     };
-    var res = await fetch('https://api-nba-v1.p.rapidapi.com/games?date=2022-11-16', options)
+    var res = await fetch(`https://api-nba-v1.p.rapidapi.com/games?date=${year}-${month}-${day}`, options)
         .catch(err => console.error(err));
 
     var data = await res.json();
-    console.log(data.response);
-    
     loadTableData(data.response)
 }
 
@@ -24,27 +28,35 @@ function loadTableData(response){
     tableData = []
 
     for(item of response){
-        tableData.push({team1: `${item.teams.home.name}`, score1: `${item.scores.home.points}`, team2: `${item.teams.visitors.name}`, score2: `${item.scores.visitors.points}` })
+
+        var homeTeam = item.teams.home.name;
+        var awayTeam = item.teams.visitors.name;
+        
+        homeTeam = homeTeam.replace(/\s+/g, '-').toLowerCase();
+        awayTeam = awayTeam.replace(/\s+/g, '-').toLowerCase();
+
+        tableData.push({team1: `${item.teams.home.name}`, score1: `${item.scores.home.points}`, team1Img: `team-logos/${homeTeam}.png`, team2: `${item.teams.visitors.name}`, score2: `${item.scores.visitors.points}`, team2Img: `team-logos/${awayTeam}.png`})
     }
 
-    for (item of tableData){
-        if(item.score1 === null || item.score2 === null){
+     for (item of tableData){
+        console.log(item)
+        console.log(item.score1)
+        if(item.score1 == 'null' || item.score2 == 'null'){
+            console.log("worked")
             item.score1 = '-'
             console.log(item.score1)
             item.score2 = '-'
         }
     }
+    
 
     console.log(tableData)
-    //let tableData = [
-        //{ team1: 'Boston Celtics', score1: 100, team2: 'Oklahome City Thunder', score2: 98}
-   //]
 
     const tableBody = document.getElementById('tableData');
     let data = '';
 
     for (item of tableData) {
-        data += `<tr><td> ${item.team1} </td><td> ${item.score1} </td><td> ${item.score2} </td><td> ${item.team2}</td></tr>`;
+        data += `<tr><td class = "left">${item.team1} <img src=${item.team1Img}></td><td> ${item.score1} </td><td> ${item.score2} </td><td class="right"><img src=${item.team2Img}>  ${item.team2}</td></tr>`;
     }
 
     console.log(data)
